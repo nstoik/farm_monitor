efa0dae504e3f1f4b9e200b588732186ff6c2736
Nelson Stoik <nelsonstoik@gmail.com>
2019-04-24 23:26:25 -0600

    updated dependencies

--- fm_server/controller/manager.py
+++ fm_server/controller/manager.py
@@ -8,8 +8,8 @@ def pika_test():
     channel = connection.channel()
     channel.queue_declare(queue='hello')
     channel.basic_publish(exchange='',
-                        routing_key='hello',
-                        body='Hello World!')
+                          routing_key='hello',
+                          body='Hello World!')
 
     print(" [x] Sent 'Hello World!'")
     time.sleep(15)
