e0a9bc13292e89ac1c25527280de2bbd65f744d1
nstoik <nelsonstoik@gmail.com>
2020-12-19 16:32:38 -0700

    fix linting on device service

---
+++ fm_server/device/device_representation.py
@@ -0,0 +1,27 @@
+"""Represent a device to a worker."""
+
+class DeviceRep(object):
+    """Helper class to represent a device to a worker."""
+
+    HEARTBEAT_LIVES = 3
+
+    def __init__(self, device_id):
+        """Create a DeviceRep object."""
+        self.device_id = device_id
+        self.lives = self.HEARTBEAT_LIVES
+        return
+
+    def heartbeat(self):
+        """Decrease live count by ."""
+        if self.lives > 0:
+            self.lives -= 1
+        return
+
+    def on_message_received(self):
+        """Set live count to max when message received."""
+        self.lives = self.HEARTBEAT_LIVES
+        return
+
+    def is_alive(self):
+        """Determine if the device is still alive."""
+        return self.lives > 0
--- fm_server/device/service.py
+++ fm_server/device/service.py
@@ -1,6 +1,7 @@
 """Device service package."""
 import json
 import logging
+from typing import Dict
 
 import pika
 from fm_database.base import get_session
@@ -8,10 +9,12 @@ from fm_database.models.device import Device
 
 from fm_server.settings import get_config
 
+from .device_representation import DeviceRep
+
 LOGGER = logging.getLogger("fm.device.service")
 
-CONNECTED_DEVICES = {}
-NEW_DEVICES = {}
+CONNECTED_DEVICES: Dict[str, DeviceRep] = {}
+NEW_DEVICES: Dict[str, DeviceRep] = {}
 
 
 class DeviceReceiver:
@@ -408,6 +411,8 @@ class HeartbeatReceiver:
 
         payload = json.loads(body)
 
+        self.LOGGER.debug(f'payload is: {payload}')
+
         device_id = properties.app_id
         if device_id in CONNECTED_DEVICES:
             CONNECTED_DEVICES[device_id].on_message_received()
@@ -700,33 +705,6 @@ class DeviceMessages:
 
         """
         self.LOGGER.debug("RabbitMQ acknowledged the cancellation of the consumer")
-
-
-class DeviceRep(object):
-    """Helper class to represent a device to a worker."""
-
-    HEARTBEAT_LIVES = 3
-
-    def __init__(self, device_id):
-        """Create a DeviceRep object."""
-        self.device_id = device_id
-        self.lives = self.HEARTBEAT_LIVES
-        return
-
-    def heartbeat(self):
-        """Decrease live count by ."""
-        if self.lives > 0:
-            self.lives -= 1
-        return
-
-    def on_message_received(self):
-        """Set live count to max when message received."""
-        self.lives = self.HEARTBEAT_LIVES
-        return
-
-    def is_alive(self):
-        """Determine if the device is still alive."""
-        return self.lives > 0
 
 
 def run_device():
