570fbf0d34755917297309ce1fc62bd405d4ae89
nstoik <nelsonstoik@gmail.com>
2020-12-19 16:48:43 -0700

    lint setup.y

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 """fm_server setup module."""
 from setuptools import find_packages, setup
 
-__version__ = '0.1'
+__version__ = "0.1"
 
 
 setup(
-    name='fm_server',
+    name="fm_server",
     version=__version__,
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
-        'click',
-        'pika',
+        "click",
+        "pika",
     ],
-    entry_points={
-        'console_scripts': [
-            'fm_server = fm_server.cli.cli:entry_point'
-        ]
-    }
+    entry_points={"console_scripts": ["fm_server = fm_server.cli.cli:entry_point"]},
 )
