6ce838367760fcf2c7150d661cb883c8223f719e
Nelson Stoik <nelsonstoik@gmail.com>
2021-01-17 19:20:27 -0700

    configure tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,20 @@
 """Define fixtures available to all tests."""
 
 import pytest
-
 from _pytest.monkeypatch import MonkeyPatch
+from fm_database.base import get_base
+from fm_database.models.system import SystemSetup
+from fm_database.models.user import User
+from fm_database.settings import get_config as get_database_config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-from fm_database.base import get_session
+# this is the same configuration of database testing as is done
+# in the fm_database module conftest.py file.
+config = get_database_config(override_default="test")
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+session = sessionmaker(bind=engine)
+db_session = scoped_session(session)
 
 
 @pytest.fixture(scope="session")
@@ -16,6 +26,7 @@ def monkeysession(request):
     mpatch = MonkeyPatch()
     yield mpatch
     mpatch.undo()
+
 
 @pytest.fixture(scope="session", autouse=True)
 def set_testing_env(monkeysession, database_env):
@@ -29,6 +40,7 @@ def set_testing_env(monkeysession, database_env):
     yield
     monkeysession.setenv("FM_SERVER_CONFIG", "dev")
 
+
 @pytest.fixture(scope="session")
 def database_env(monkeysession):
     """Set the fm_database env variable for testing and set back when done.."""
@@ -36,7 +48,8 @@ def database_env(monkeysession):
     yield
     monkeysession.setenv("FM_DATABASE_CONFIG", "dev")
 
-@pytest.fixture
+
+@pytest.fixture(scope="session")
 def dbsession():
     """Returns an sqlalchemy session."""
 
@@ -46,9 +59,28 @@ def dbsession():
 @pytest.fixture
 def tables(dbsession):
     """Create all tables for testing. Delete when done."""
-    base = get_base(with_query=True)
+    base = get_base()
     base.query = dbsession.query_property()
     base.metadata.create_all(bind=engine)
     yield
     dbsession.close()
     base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def database_base_seed(dbsession, tables):
+    """Enter the base configuration data into the database."""
+
+    system = SystemSetup()
+    dbsession.add(system)
+
+    user = User(
+        username="admin",
+        email="admin@mail.com",
+        password="admin",
+        active=True,
+        is_admin=True,
+    )
+    dbsession.add(user)
+
+    dbsession.commit()
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,6 +1,9 @@
 """Test functional aspects of the app."""
 
-from fm_database.settings import TestConfig as DBTestConfig, get_config as db_get_config
+from fm_database.models.system import SystemSetup
+from fm_database.models.user import User
+from fm_database.settings import TestConfig as DBTestConfig
+from fm_database.settings import get_config as db_get_config
 
 from fm_server.settings import TestConfig, get_config
 
@@ -11,8 +14,28 @@ def test_main_env():
     config = get_config()
     assert config == TestConfig
 
+
 def test_database_env():
     """Test that the database environment is properly set using a fixture."""
 
     config = db_get_config()
     assert config == DBTestConfig
+
+
+def test_database_setup_for_testing(database_base_seed, dbsession):
+    """Test that the database base seed is correctly added to the database."""
+
+    system_setup = SystemSetup.query.first()
+
+    assert not bool(system_setup.first_setup_complete)
+    assert not bool(system_setup.update_in_progress)
+    assert not bool(system_setup.new_update_installed)
+    assert system_setup.first_setup_time is None
+
+    user = User.query.filter_by(username="admin").first()
+
+    assert user.username == "admin"
+    assert user.email == "admin@mail.com"
+    assert user.check_password("admin")
+    assert bool(user.active)
+    assert bool(user.is_admin)
