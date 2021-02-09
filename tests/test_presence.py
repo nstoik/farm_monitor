420bfec02d7ce36581dbb753d2d44e3cdbaf1032
Nelson Stoik <nelsonstoik@gmail.com>
2021-02-08 22:53:08 -0700

    add tests for presence service

--- tests/test_presence.py
+++ tests/test_presence.py
@@ -1,13 +1,24 @@
 """Test the presence service."""
+import time
+from multiprocessing import Process
+
+import pytest
+
+from fm_server.presence import presence_service
 
 
-class TestPresenceService:
-    """Presence service tests."""
+@pytest.mark.usefixtures("database_base_seed")
+def test_presence_start():
+    """Test that the presence service starts."""
 
-    @staticmethod
-    def test_presence_start():
-        """TBD."""
+    presence_controller = Process(target=presence_service)
+    presence_controller.start()
 
-    @staticmethod
-    def test_presence_stop():
-        """TBD."""
+    time.sleep(1)
+
+    presence_controller.terminate()
+    presence_controller.join()
+
+    # exitcode is the negative of the signal used to terminate
+    # the process. 15 is SIGTERM
+    assert presence_controller.exitcode == -15
