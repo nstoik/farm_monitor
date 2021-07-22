c2427d6b1f391ba35636ce43fc11132df1408392
Nelson Stoik <nelsonstoik@gmail.com>
2021-07-22 17:57:14 -0600

    add database app

--- database/setup.py
+++ database/setup.py
@@ -9,6 +9,12 @@ setup(
     name="fm_database",
     version=__version__,
     packages=find_packages(exclude=["tests"]),
-    install_requires=["click", "sqlalchemy", "passlib", "psycopg2"],
+    install_requires=[
+        "click>=8",
+        "sqlalchemy>=1.4",
+        "passlib",
+        "psycopg2",
+        "alembic>=1.6",
+    ],
     entry_points={"console_scripts": ["fm_database = fm_database.cli.cli:entry_point"]},
 )
