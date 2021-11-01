a83b333d94815403b6920c4c43796b6bbf18c64c
Nelson Stoik <nelsonstoik@gmail.com>
2021-10-31 22:52:15 -0600

    add DeviceUpdate schema, views and tests

--- api/fm_api/device/views.py
+++ api/fm_api/device/views.py
@@ -1,14 +1,17 @@
 """Views for Device API."""
 from flask import current_app
 from flask.views import MethodView
-from flask_jwt_extended import jwt_required
-from flask_smorest import Blueprint, abort
-from fm_database.models.device import Device
 
-from .schemas import DeviceSchema
+# from flask_jwt_extended import jwt_required
+from flask_smorest import Blueprint, abort
+from flask_smorest.pagination import PaginationParameters
+from fm_database.models.device import Device, DeviceUpdate
+from fm_database.paginate import Pagination
+
+from .schemas import DeviceSchema, DeviceUpdateSchema
 
 blueprint = Blueprint(
-    "api_device",
+    "device",
     "device",
     url_prefix="/api/devices",
     description="Operations on devices",
@@ -53,3 +56,51 @@ class DevicesById(MethodView):
         if item is None:
             abort(404, message=f"Device with device id: {device_id} not found.")
         return item
+
+
+@blueprint.route("/<device_id>/updates")
+class DeviceUpdates(MethodView):
+    """MethodView for DeviceUpdate schema that require an ID."""
+
+    # decorators = [jwt_required()]
+
+    @staticmethod
+    @blueprint.response(200, DeviceUpdateSchema(many=True))
+    @blueprint.paginate()
+    def get(device_id, pagination_parameters: PaginationParameters):
+        """Get DeviceUpdates for a given Device ID with Pagination.
+
+        Default pagination parameters are 10 per page starting at page 1.
+        Ordered by most recent updates first.
+        """
+
+        devices: Pagination = (
+            DeviceUpdate.query.filter_by(device_id=device_id)
+            .order_by(DeviceUpdate.update_index.desc())
+            .paginate(pagination_parameters.page, pagination_parameters.page_size)
+        )
+
+        pagination_parameters.item_count = devices.total
+        return devices.items
+
+
+@blueprint.route("/<device_id>/updates/latest")
+class DeviceUpdatesLatest(MethodView):
+    """MethodView for DeviceUpdate schema that require an ID."""
+
+    # decorators = [jwt_required()]
+
+    @staticmethod
+    @blueprint.response(200, DeviceUpdateSchema)
+    def get(device_id):
+        """Get the latest DeviceUpdate for a given Device ID."""
+
+        device_update = (
+            DeviceUpdate.query.filter_by(device_id=device_id)
+            .order_by(DeviceUpdate.update_index.desc())
+            .first()
+        )
+
+        if device_update is None:
+            abort(404, message=f"Device with device id: {device_id} not found.")
+        return device_update
--- api/tests/api/device/test_views.py
+++ api/tests/api/device/test_views.py
@@ -1,6 +1,10 @@
 """Test the API Device views."""
+import datetime as dt
+import json
+
 import pytest
 from flask import url_for
+from fm_database.models.device import DeviceUpdate
 
 from ...factories import DeviceFactory
 
@@ -28,14 +32,14 @@ class TestAPIDevices:
     def test_api_device_create(flaskclient, auth_headers):
         """Test that a new device can be created with required fields."""
 
-        json = {
+        custom_json = {
             "device_id": "my-device-id",
             "name": "my-device-name",
             "hardware_version": "v0.1",
             "software_version": "v0.2",
         }
         url = url_for("device.Devices")
-        rep = flaskclient.post(url, json=json, headers=auth_headers)
+        rep = flaskclient.post(url, json=custom_json, headers=auth_headers)
 
         returned_device = rep.get_json()
         print(returned_device)
@@ -85,3 +89,68 @@ class TestAPIDevicesById:
 
         assert rep.status_code == 404
         assert message["message"] == "Device with device id: 5 not found."
+
+
+@pytest.mark.usefixtures("tables")
+class TestAPIDeviceUpdates:
+    """Test the API DeviceUpdates views."""
+
+    @staticmethod
+    def test_api_device_updates_get(flaskclient, auth_headers, dbsession):
+        """Test that a device update is returned by ID."""
+
+        device = DeviceFactory()
+        device.save()
+
+        # create a bunch of DeviceUpdates
+        for x in range(25):
+            device_update = DeviceUpdate(device.id)
+            device_update.timestamp = dt.datetime.now()
+            device_update.update_index = x
+            dbsession.add(device_update)
+
+        dbsession.commit()
+
+        url = url_for("device.DeviceUpdates", device_id=device.id)
+        rep = flaskclient.get(url, headers=auth_headers)
+        fetched_device_update = rep.get_json()
+
+        assert rep.status_code == 200
+        assert len(fetched_device_update) == 10
+
+    @staticmethod
+    def test_api_device_updates_empty_for_no_device(flaskclient, auth_headers):
+        """Test that a no DeviceUpdates is returned for non-existent device."""
+
+        url = url_for("device.DeviceUpdates", device_id=5)
+        rep = flaskclient.get(url, headers=auth_headers)
+        message = rep.get_json()
+
+        assert rep.status_code == 200
+        assert len(message) == 0
+
+    @staticmethod
+    def test_api_device_updates_pagination_header(flaskclient, auth_headers, dbsession):
+        """Test that the pagination header is present and accurate."""
+
+        device = DeviceFactory()
+        device.save()
+
+        # create a bunch of DeviceUpdates
+        for x in range(25):
+            device_update = DeviceUpdate(device.id)
+            device_update.timestamp = dt.datetime.now()
+            device_update.update_index = x
+            dbsession.add(device_update)
+
+        dbsession.commit()
+
+        url = url_for("device.DeviceUpdates", device_id=device.id)
+        rep = flaskclient.get(url, headers=auth_headers)
+        returned_header = json.loads(rep.headers.get("X-Pagination"))
+        fetched_device_update = rep.get_json()
+
+        assert rep.status_code == 200
+        assert len(fetched_device_update) == 10
+        assert returned_header["total"] == 25
+        assert returned_header["total_pages"] == 3
