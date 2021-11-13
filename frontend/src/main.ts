2adcf5d01fa79028b9903ec176168306741130d1
Nelson Stoik <nelsonstoik@gmail.com>
2021-11-12 22:02:05 -0700

    add Bootstrap to main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
+
+import "@popperjs/core";
+import "bootstrap";
