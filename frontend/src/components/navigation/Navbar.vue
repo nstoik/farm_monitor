082ae757db8176fb4ca5f1ff45bd03b380186e67
Nelson Stoik <nelsonstoik@gmail.com>
2021-11-12 22:03:26 -0700

    create simple navbar

---
+++ frontend/src/components/navigation/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="nav">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand" exact-active-class=""
+          >Farm Monitor</router-link
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+#nav {
+  padding: 35px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #c1c7ce;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
