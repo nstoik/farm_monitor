ee307519604d48ee0b0802130cda6bc8676e443c
Nelson Stoik <nelsonstoik@gmail.com>
2021-11-14 22:12:00 -0700

    fix url and convert dates from strings

--- frontend/src/api/device.api.ts
+++ frontend/src/api/device.api.ts
@@ -4,13 +4,18 @@ import { Device } from "@/interfaces/device.interface";
 export class DeviceRequest extends Request {
   constructor() {
     super();
-    this.resourceLocation = `${this.baseURL}devices`;
+    this.resourceLocation = `${this.baseURL}devices/`;
   }
 
   public async getDevices(): Promise<Array<Device>> {
     const url = `${this.resourceLocation}`;
-    return this.client.get(url).then((response) => response.data);
+    return this.client.get(url).then((response) => {
+      // convert any dates from strings to Date objects
+      response.data.forEach((device: Device) => {
+        device.lastUpdateReceived = new Date(device.lastUpdateReceived);
+        device.lastUpdated = new Date(device.lastUpdated);
+      });
+      return response.data;
+    });
   }
 }
-
-export default new DeviceRequest();
