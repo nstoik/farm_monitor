05aa2fbf17f02e587025f1976037299388d52a1d
Nelson Stoik <nelsonstoik@gmail.com>
2021-11-21 15:06:06 -0700

    initial Grainbin views

---
+++ frontend/src/components/grainbin/Grainbin.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Grainbins</h2>
+  <div v-if="grainbins.length == 0" class="row">
+    <h3>No grainbins found</h3>
+  </div>
+</template>
+
+<script lang="ts" , setup>
+import { ref } from "vue";
+
+import { Grainbin } from "@/interfaces/grainbin.interface";
+
+const grainbins = ref<Array<Grainbin>>([]);
+</script>
+>
---
+++ frontend/src/interfaces/grainbin.interface.ts
@@ -0,0 +1,14 @@
+export type Grainbin = {
+  averageTemp: string;
+  userConfigured: boolean;
+  busNumber: number;
+  lastUpdated: Date;
+  device: number;
+  name: string;
+  sensorType: string;
+  totalUpdates: number;
+  description: string;
+  url: string;
+  busNumberString: string;
+  grainbinType: string;
+};
