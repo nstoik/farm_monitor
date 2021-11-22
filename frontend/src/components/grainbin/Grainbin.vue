f5bf9e04ea03daf79a1ccf99a6668e722e7d2851
Nelson Stoik <nelsonstoik@gmail.com>
2021-11-21 22:49:30 -0700

    add grainbin card and grainbin update component

--- frontend/src/components/grainbin/Grainbin.vue
+++ frontend/src/components/grainbin/Grainbin.vue
@@ -3,13 +3,34 @@
   <div v-if="grainbins.length == 0" class="row">
     <h3>No grainbins found</h3>
   </div>
+  <div
+    v-for="grainbin in grainbins"
+    :key="grainbin.id"
+    class="row align-items-start g-3 mb-3"
+  >
+    <div class="col-md-12 col-lg-6 col-xl-3">
+      <grainbin-card :grainbin="grainbin" />
+    </div>
+    <div class="col-md-12 col-lg-6 col-xl-3">
+      <grainbin-update :grainbinID="grainbin.id"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" , setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
+import { GrainbinRequest } from "@/api/grainbin.api";
 import { Grainbin } from "@/interfaces/grainbin.interface";
+import GrainbinCard from "@/components/grainbin/GrainbinCard.vue";
+import GrainbinUpdate from "@/components/grainbin/GrainbinUpdate.vue";
 
+const grainbinAPI = new GrainbinRequest();
 const grainbins = ref<Array<Grainbin>>([]);
+
+onMounted(() => {
+  grainbinAPI.getGrainbins().then((response) => {
+    grainbins.value = response;
+  });
+});
 </script>
->
