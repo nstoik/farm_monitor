34d4f87ec333a13aa2b2ffb55fd5085e4bdd17be
Nelson Stoik <nelsonstoik@gmail.com>
2022-07-29 19:19:10 -0600

    layout changes (#266)
    * add divider between items
    
    * add Device button to navbar
    
    * add device to routes
    
    * create separate deviceview page

--- frontend/src/components/navigation/NavbarMain.vue
+++ frontend/src/components/navigation/NavbarMain.vue
@@ -22,6 +22,9 @@
               <router-link to="/" class="nav-link">Home</router-link>
             </li>
             <li class="nav-item">
+              <router-link to="/device" class="nav-link">Devices</router-link>
+            </li>
+            <li class="nav-item">
               <router-link to="/about" class="nav-link">About</router-link>
             </li>
             <li>
---
+++ frontend/src/views/DeviceView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="device">
+    <div class="container">
+      <device></device>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Device from "@/components/device/DeviceMain.vue";
+</script>
