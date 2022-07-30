020dae45b13d6a7d3308319603118af300ac5cbb
Nelson Stoik <nelsonstoik@gmail.com>
2022-07-29 18:47:19 -0600

    create separate deviceview page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,12 +2,10 @@
   <div class="home">
     <div class="container">
       <grainbin></grainbin>
-      <device></device>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Device from "@/components/device/DeviceMain.vue";
 import Grainbin from "@/components/grainbin/GrainbinMain.vue";
 </script>
