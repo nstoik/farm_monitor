57a2b48ddb7b167cc0b262dbbcfc72a54b264a0e
Nelson Stoik <nelsonstoik@gmail.com>
2024-02-16 19:34:36 -0700

    reorganize tests

---
+++ frontend/tests/components/grainbin/GrainbinMain.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from "@vue/test-utils"
+import GrainbinMain from '../../../src/components/grainbin/GrainbinMain.vue'
+
+describe('GrainbinMain', () => {
+  it('renders properly', () => {
+    const wrapper = mount(GrainbinMain)
+    expect(wrapper.text()).toContain('No grainbins found')
+  })
+})
