b7521fe1c6bda5efd5be3f981727baa16f1bae72
Nelson Stoik <nelsonstoik@gmail.com>
2024-03-03 01:38:31 -0700

    don't exclude id

--- api/fm_api/device/schemas.py
+++ api/fm_api/device/schemas.py
@@ -33,7 +33,7 @@ class DeviceUpdateSchema(BaseSchema):
     class Meta:  # pylint: disable=too-few-public-methods
         """Meta configuration for DeviceUpdateSchema."""
 
-        exclude = ("id",)
+        # exclude = ("id",)
 
         model = DeviceUpdate
         include_relationships = True
